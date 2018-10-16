755136293ae588384310356b38a52f06a0310ea1
Guillaume Bozon <guigui_le_lionnais@hotmail.fr>
2018-10-16 09:39:39 +0000

    Upload New File

---
+++ Direction.java
@@ -0,0 +1,7 @@
+// 5/10/2018 - Guillaume Bozon
+// Enum permettant de gerer les differentes directions du joueur sur la case
+// Version : 1.0.0
+
+public enum Direction {
+	NORTH, SOUTH, EAST, WEST;
+}
