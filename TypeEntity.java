5c6b98448bc6fecb699647348635fb5d928f4b51
Guillaume Bozon <guigui_le_lionnais@hotmail.fr>
2018-10-16 09:41:30 +0000

    Upload New File

---
+++ TypeEntity.java
@@ -0,0 +1,12 @@
+// 5/10/2018 - Guillaume Bozon
+// Classe permettant de savoir le type d'objet sur la case
+// Version : 1.2.0
+
+public enum TypeEntity {
+	PLAYER,
+	MONSTER,
+	BOMB,
+	BRICK,
+	WALL,	
+	EMPTY;
+}
